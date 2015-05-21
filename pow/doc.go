3ef940c82c0181d5e92955aa2c0f50ac151e9473
Ishbir Singh <ishbir@ishbir.com>
2015-05-21 18:17:19 +0530

    Added PoW check and do (sequential and parallel).

--- pow/doc.go
+++ pow/doc.go
@@ -3,6 +3,9 @@
 // license that can be found in the LICENSE file.
 
 /*
-Package pow implements proof-of-work and its checking mechanism.
+Package pow implements proof-of-work and its checking mechanism. More details
+for PoW for Bitmessage can be found at:
+
+https://bitmessage.org/wiki/Proof_of_work
 */
 package pow
